23c07d728000dab3a21027e8cb3f5e0e5be36acc
Hsiang-En <hsiangen@ewaytech.tw>
2025-04-19 23:14:55 +0800

    Add tools
    add my own tools
    
    Change-Id: I2b27b717420e9a364c5fb0966e8f0706e2032f64

--- queue.c
+++ queue.c
@@ -1,28 +1,101 @@
+#include "queue.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+    struct list_head head; /* 環狀鏈結哨兵 */
+    size_t size;           /* 佇列長度，方便 O(1) 取值 */
+} queue_t;
 
-#include "queue.h"
+/* 方便透過 head 找回 queue_t */
+static inline queue_t *to_queue(struct list_head *head)
+{
+    return container_of(head, queue_t, head);
+}
+
+/* ---------- 基本工具 ---------- */
+
+static inline void list_swap_pair(struct list_head *first)
+{
+    struct list_head *second = first->next;
+    list_del(second);
+    list_add(second, first->prev); /* 插到 first 前面 */
+}
+
+/* 反轉 [first, last]（包含兩端），不包含 head。
+ * 傳回反轉後的新起點（原本的 last）。 */
+static struct list_head *reverse_segment(struct list_head *first,
+                                         struct list_head *last)
+{
+    struct list_head *cur = first, *nxt;
+    /* 迭代到 last (包含) 為止 */
+    while (cur != last) {
+        nxt = cur->next;
+        list_move(cur, last); /* 每次把 cur 移到 last 之後 */
+        cur = nxt;
+    }
+    return last; /* 此時 last 已變成 segment 新起點 */
+}
+
+element_t *create_new_element(char *s)
+{
+    element_t *new_element = malloc(sizeof(element_t));
+    if (!new_element)
+        return NULL;
+    new_element->value = strdup(s);
+    if (!new_element->value) {
+        free(new_element);
+        return NULL;
+    }
+    return new_element;
+}
 
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    queue_t *q = malloc(sizeof(queue_t));
+    if (!q)
+        return NULL;
+    INIT_LIST_HEAD(&q->head);
+    q->size = 0;
+    return &q->head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    while (!list_empty(head)) {
+        element_t *e = list_first_entry(head, element_t, list);
+        list_del(&e->list);
+        q_release_element(e);
+    }
+    free(to_queue(head));
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *new_element = create_new_element(s);
+    if (!new_element)
+        return false;
+    list_add(&new_element->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *new_element = create_new_element(s);
+    if (!new_element)
+        return false;
+    list_add_tail(&new_element->list, head);
     return true;
 }
 
